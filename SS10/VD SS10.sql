9bf79699b9566e6423f543e0376e537e6f983644
nguyenmanhkien <91303469+nguyenmanhkien@users.noreply.github.com>
2022-01-07 10:13:19 +0700

    Update VD SS10.sql

--- SS10/VD SS10.sql
+++ SS10/VD SS10.sql
@@ -1,5 +1,103 @@
 USE AdventureWorks2019
 GO
+
+-- 1:
+CREATE VIEW vwProductInfo AS
+SELECT ProductID, ProductNumber, Name ,SafetyStockLevel 
+FROM Production. Product;
+GO
+
+-- 2:
+| SELECT * FROM vwProductInfo
+
+-- 3:
+CREATE VIEW wwPersonDetails AS
+SELECT 
+p.Title
+,p.[FirstName]
+,p.[MiddleName]
+,p.[LastName]
+,e.[JobTitle]
+FROM[HumanResources].[Employee]e
+	INNER JOIN [Person].[Person]p
+	ON p.[BusinessEntityID] = e.[BusinessEntityID]
+GO
+
+-- 4:
+| SELECT * FROM vwPersonDetails
+
+-- 5:
+CREATE VIEW wwPersonDetailsNew
+AS
+SELECT 
+COALESCE(p.MiddleName,' ')AS  Title
+,p.[FirstName]
+ ,COALESCE(p.MiddleName,' ')ASMiddleName
+ ,p.[LastName]
+ ,e.[JobTitle]
+ FROM[HumanResources].[Employee]e
+	INNER JOIN [Person].[Person]p
+	ON p.[BusinessEntityID] = e.[BusinessEntityID]
+GO
+
+-- 6:
+CREATE VIEW wwSortedPersonDetails AS
+SELECT TOP 10 COALESCE(p.Title,'') AS Title
+,p.[FirstName]
+COALESCE(p.MiddleName,' ')AS  Title
+,p.[FirstName]
+ ,e.[JobTitle]
+ FROM [HumanResources].[Employee] e INNER JOIN [Person].[Person] p
+ ON p.[BusinessEntityID] = e.[BusinessEntityID] ORDER BY p.FirstName
+ GO
+ 
+ SELECT * FROM  wwSortedPersonDetails
+
+ -- 7:
+ CREATE TABLE Employee_Personal_Details(
+ EmpID int NOT NULL,
+ FirstName varchar(30)NOT NULL,
+ lastName varchar(30)NOT NULL, Address
+ varchar (30)
+ )
+
+ -- 8:
+CREATE TABLE Employee_Salary_Details(
+EmpID
+int NOT NULL,
+Designation varchar(30),
+Salary int NOT NULL
+)
+-- 9
+CREATE VIEW vwEmployee_Personal_Details
+AS
+SELECT e1.EmpID, FirstName, LastName, Designation, Salary
+FROM Employee_Personal_Details e1
+JOIN Employee_Salary_Details e2
+ON e1.EmpID=e2.EmpID
+-- 10
+INSERT INTO vwEmployee_Personal_Details VALUES (2,'Jack','Wilson','Software
+Developer',16000)
+-- 11
+CREATE VIEW vwEmpDetails AS
+SELECT FirstName, Address
+FROM Employee_Personal_Details
+-- 12
+INSERT INTO vwEmpDetails VALUES ('Jack', 'NYC')
+-- 13
+CREATE TABLE Product_Details (
+ProductID int, ProductName
+varchar(30), Rate money
+)
+-- 14
+CREATE VIEW vwProduct_Details
+AS
+SELECT ProductName, Rate FROM Product_Details
+-- 15
+UPDATE vwProduct_Details
+SET Rate=3000
+WHERE ProductName='DVD Writer'
+
 --16
 CREATE VIEW vwProduct_Details
 AS
@@ -49,3 +147,37 @@ GO
 -- 24
 UPDATE vwProductInfo SET SafetyStockLevel =2500
 WHERE ProductID=321
+
+-- 25
+create view vwNewProductInfo with schemabinding as 
+select ProductID, ProductNumber, Name, SafetyStockLevel 
+from Production.Product;
+go
+
+-- 26
+create table vwCustomers
+(
+CustID int,
+CustName varchar(50),
+Address varchar(60)
+)
+
+-- 27
+create view viewCustomers
+as
+select * from vwCustomers 
+
+-- 28
+select * from vwCustomers
+
+-- 29
+alter table vwCustomers ADD Age int
+
+-- 30
+select * from vwCustomers
+
+-- 31
+EXEC sp_refreshview 'vwCustomers'
+
+-- 32 
+alter table Production.Product alter column ProductID varchar(7)
